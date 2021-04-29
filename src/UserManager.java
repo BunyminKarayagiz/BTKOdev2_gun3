2b22caa2faa42fdaf7feffdbfcd95363ecbea99b
BunyminKarayagiz <82951656+BunyminKarayagiz@users.noreply.github.com>
2021-04-30 02:55:18 +0300

    Update UserManager.java

--- src/UserManager.java
+++ src/UserManager.java
@@ -3,10 +3,23 @@ public class UserManager {
 	
 	
 	public void giris(User user) {
-		System.out.println("=============================="+user.getSchoolName()+ " Resmi Sayfasıdır =======================================");
-		System.out.println("Üniversiteye Giriş Yaptı:");
-		System.out.println(">>> Giriş Yapan Kişinin Bilgileri: <<<");
+		System.out.println("=============================="+user.getSchoolName()+ " Resmi SayfasÄ±dÄ±r =======================================");
+		System.out.println("Ãœniversiteye GiriÅŸ YaptÄ±:");
+		System.out.println(">>> GiriÅŸ Yapan KiÅŸinin Bilgileri: <<<");
 		
+	}
+	
+	//Burada Veriler Silinmektedir.
+	public void delete(User userDelete) {
+		
+		System.out.println(">>>>>>>> Kullanici Silindi <<<<<<<< ");
+		System.out.println("\n Silinen Kullanicinin Ismi ve Soyismi: ");
+		System.out.println("Isim: "+userDelete.getName());
+		System.out.println("Soyisim: "+userDelete.getSurName()+"\n\n\n");
+		userDelete.seteMail(null);
+		userDelete.setFaculty(null);
+		userDelete.setName(null);
+		userDelete.setSurName(null);
 	}
 
 }
