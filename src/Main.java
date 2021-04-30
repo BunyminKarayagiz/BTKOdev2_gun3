410867e7bdaafa199a10d1f41d7998a3c44de082
BunyminKarayagiz <82951656+BunyminKarayagiz@users.noreply.github.com>
2021-04-30 18:48:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,13 @@ public class Main {
 		Student Bunyamin=new Student("Bünyamin","Karayaðýz","Mühendislik Fakültesi","bkz@gmail.com","123456798",12,"Bilgisayar Mühendisliði");
 		InstructorManager instructorManager=new InstructorManager();
 		StudentManager studentManager=new StudentManager();
+		
 		instructorManager.giris(Engin);
+		instructorManager.delete(Engin);
+		
 		studentManager.giris(Bunyamin);
+		studentManager.delete(Bunyamin);
+		
 		
 
 	}
